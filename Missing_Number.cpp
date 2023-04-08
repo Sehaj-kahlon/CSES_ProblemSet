55a53b8fec832be513af0eaf34c3d89b4ef9ef64
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-08 11:03:40 +0530

    missing numbers

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -1,11 +1,27 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-const int n1=1e9+7;
+const int n1 = 1e9 + 7;
 #define ll long long
- 
+
 int main()
 {
-   
-   return 0;
+    long long n;
+    cin >> n;
+    long long arr[n - 1];
+    for (long long i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != i + 1)
+        {
+            cout << i + 1 << endl;
+            break;
+        }
+    }
+
+    return 0;
 }
