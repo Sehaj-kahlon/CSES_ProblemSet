3b59cd884fed9bd1e56eae41270e84b16d82522a
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-08 10:43:47 +0530

    missing numbers

---
+++ Missing_Number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+const int n1=1e9+7;
+#define ll long long
+ 
+int main()
+{
+   
+   return 0;
+}
--- Wierd_ALgorithm.cpp
+++ Wierd_ALgorithm.cpp
@@ -1,11 +1,26 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-const int n1=1e9+7;
+const int n1 = 1e9 + 7;
 #define ll long long
- 
+
 int main()
 {
-   
-   return 0;
+    long long  n;
+    cin >> n;
+    cout<< n <<" ";
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+            cout << n << " ";
+        }
+        else
+        {
+            n = n * 3 + 1;
+            cout << n << " ";
+        }
+    }
+    return 0;
 }
