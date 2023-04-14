e07eb53a5bf635de02ee077bd69c938a42eb659f
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-14 20:36:14 +0530

    trailing seros solution

--- Trailing_Zeros.cpp
+++ Trailing_Zeros.cpp
@@ -3,25 +3,15 @@
 using namespace std;
 const int n1 = 1e9 + 7;
 #define ll long long
-ll fact(ll n)
-{
-    if (n <= 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return n * fact(n - 1);
-    }
-}
 int main()
 {
     ll n;
     cin >> n;
-    ll ans = 0;
-    for (ll i = 1; i <= n; i++)
+    ll ans = 0, temp =5;
+    while(temp<=n)
     {
-        ans += n / pow(5, i);
+        ans += n /temp;
+        temp *=5;
     }
     cout << ans << endl;
     return 0;
