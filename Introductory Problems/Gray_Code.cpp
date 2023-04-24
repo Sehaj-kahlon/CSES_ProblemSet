15aff58129c0f53eed278daadbcef873c5ceb8a4
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-25 03:43:35 +0530

    gray codes

--- Introductory Problems/Gray_Code.cpp
+++ Introductory Problems/Gray_Code.cpp
@@ -3,26 +3,46 @@
 using namespace std;
 const int n1 = 1e9 + 7;
 #define ll long long
-int powf(int x, int n)
+void generateGrayarr(int n)
 {
-    if (n == 0)
+    if (n <= 0)
     {
-        return 1;
+        return;
     }
-    else
+    vector<string> arr;
+    // intitally making the array of 0 1 - starting from 0 1
+    arr.push_back("0");
+    arr.push_back("1");
+    int i, j;
+    for (int i = 2; i < (1 << n); i = i << 1)
     {
-        n--;
-        return x * pow(x, n);
+        // entering the previosly generated code in the array again like l2
+        for (j = i - 1; j >= 0; j--)
+        {
+            arr.push_back(arr[j]);
+        }
+        // append 0 to the first half
+        for (j = 0; j < i; j++)
+        {
+            arr[j] = "0" + arr[j];
+        }
+        // appendn 1 to the second half
+        for (j = i; j < 2 * i; j++)
+        {
+            arr[j] = "1" + arr[j];
+        }
+    }
+    for (i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << endl;
     }
 }
 int main()
 {
     int n;
     cin >> n;
-    int f = powf(2, n);
-    // for(int i = 0; i < f; i++){
 
-    // }
+    generateGrayarr(n);
 
     return 0;
 }
